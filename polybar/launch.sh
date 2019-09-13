55d94bee6feba72386948419c6acdfe2ea998c0b
Loïs Burg <lois.burg@protonmail.com>
2019-09-13 19:52:00 +0200

    polybar: Fix config file path location in launcher

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,8 +3,8 @@
 # Launch polybar on each connector monitor
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --config=~/.polybar/config --reload arch &
+    MONITOR=$m polybar --config=$HOME/.polybar/config --reload arch &
   done
 else
-  polybar --config=~/.polybar/config --reload arch &
+  polybar --config=$HOME/.polybar/config --reload arch &
 fi

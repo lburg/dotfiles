0cab2d1ca3a775a844376e8a3b555a18d33a8433
Loïs Burg <lois.burg@protonmail.com>
2021-03-08 12:47:51 +0100

    config: Add zoom_toggle kitten
    This allows zooming in and out of a Kitty terminal window.

---
+++ config/kitty/zoom_toggle.py
@@ -0,0 +1,14 @@
+# Taken from https://sw.kovidgoyal.net/kitty/kittens/custom.html#using-kittens-to-script-kitty-without-any-terminal-ui
+
+def main(args):
+    pass
+
+from kittens.tui.handler import result_handler
+@result_handler(no_ui=True)
+def handle_result(args, answer, target_window_id, boss):
+    tab = boss.active_tab
+    if tab is not None:
+        if tab.current_layout.name == 'stack':
+            tab.last_used_layout()
+        else:
+            tab.goto_layout('stack')
